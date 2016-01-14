4c54082134e9a1ea32a28b7398ff9f1bb017b9e4
Johnny Pacia <johnny.pacia@gmail.com>
2016-01-14 08:52:22 -0500

    johnny pacia hw w13d03

--- app.js
+++ app.js
@@ -7,9 +7,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
+var db;
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/birds", function(err, database){
+    if (err) { console.log(err) }
+    db = database;
+}); 
+
+
 // Routes
 app.get('/', function(req, res){
   res.render('index');
 });
+
+app.get('/sightings/new', function(req, res){
+	res.render('form');
+});
+
+app.post('/sightings', function(req, res){
+	console.log(req.body.sighting.location);
+	db.collection('sightings').insert( {bird: req.body.sighting.bird, location: req.body.sighting.location,  date: req.body.sighting.date }, function(err, result){
+		console.log(result);
+		res.redirect('/');
+	})
+});
+
+app.get('/api/sightings', function(req, res){
+	db.collection('sightings').find({}).toArray(function(err, results){
+		res.json(results);
+	})
+});
+
 
 app.listen(process.env.PORT || 3000);
--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,22 @@ console.log("        <')_,/ ")
 console.log("        (_==/  ")
 console.log("birder  ='-    ")
 console.log("we're hiring!  ")
+
+
+
+$(document).ready(function(){
+
+	$.ajax({
+		url: '/sightings',
+		method:  'GET',
+		dataType: 'json'
+	}).done(function(data){
+		console.log(data);
+		callback(data);
+	});
+
+	var source = $('#list-template').html();
+	var template = Handlebars.compile(source);
+	console.log(template);
+
+});
